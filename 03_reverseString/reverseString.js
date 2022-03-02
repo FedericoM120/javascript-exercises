7a876bc66cacd1140a2c91c9ed0c331e402fa4c1
Federico Monteverdi <monteverdi.102@gmail.com>
2022-03-01 21:08:37 -0600

    added different psuedo code with knew knowledge that strings can be indexed without arrays

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,18 @@
-const reverseString = function(string) {
-    //create empty array
-    //assign string to empty array
+const reverseString = function(sentence) {
+    //assign string to variable
+    //get length of string from variable
     //assign index to each letter, number, or puncuation of string
     //create for loop that counts backward
     //join the strings from greatest index to least
+
+    //assign sentence to string variable
+    let string = sentence;
+    //get string length
+    let length = string.length;
+    //create for loop counting backwards
+    
+    
+
 };
 
 // Do not edit below this line

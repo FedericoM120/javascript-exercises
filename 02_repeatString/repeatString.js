859fbb49a1dd422551be47b9878036d6da3dcc9f
Federico Monteverdi <monteverdi.102@gmail.com>
2022-03-01 20:16:34 -0600

    function passes alll tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,9 +2,11 @@ const repeatString = function(string, num) {
     const hey = [];
     for (let i = 0; i < num; i++) {
         hey.push(string);
+    } if (num < 0) {
+        return "ERROR";
     }
-    return hey.toString();
+    return hey.join('');
 }
-la la la
+
 // Do not edit below this line
 module.exports = repeatString;

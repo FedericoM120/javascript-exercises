62403e544cf385eda4136ebcf7ca3ee144340ca5
Federico Monteverdi <monteverdi.102@gmail.com>
2022-03-01 20:37:04 -0600

    wrote out psuedo code for function reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //create empty array
+    //assign string to empty array
+    //assign index to each letter, number, or puncuation of string
+    //create for loop that counts backward
+    //join the strings from greatest index to least
 };
 
 // Do not edit below this line

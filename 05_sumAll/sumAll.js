365d72627c7c405e94a3537ee8debad498683265
Federico Monteverdi <monteverdi.102@gmail.com>
2022-03-15 16:45:24 -0500

    function returns sum of numbers with arguement one being larger than arguement two and vice versa

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,24 +1,26 @@
-/*const sumAll = function(numberOne, numberTwo) {
+const sumAll = function(numberOne, numberTwo) {
     var sum = 0;
     if (numberOne < numberTwo) {
         for (let i = numberOne; i <= numberTwo; i++) {
             sum += i;
         };
-    }; else (numberOne > numberTwo) {
+    } else if (numberOne > numberTwo) {
         for (let i = numberOne; i >= numberTwo; i--) {
             sum += i;
         };
-    };
+    } else if (numberOne < 0 || numberTwo < 0) {
+        return "ERROR";
+    }
     return sum;
-};*/
+};
 
-const sumAll = function(numberOne, numberTwo) {
+/*const sumAll = function(numberOne, numberTwo) {
     var sum = 0;
     for (let i = numberOne; i <= numberTwo; i++) {
         sum += i;
         };
         return sum;
-};
+};*/
 
 // Do not edit below this line
 module.exports = sumAll;

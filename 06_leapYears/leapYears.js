eac15e73e78c8cfb25cff292bf42599b9a83821c
Federico Monteverdi <monteverdi.102@gmail.com>
2022-03-22 22:26:43 -0500

    function correctly checks if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -3,13 +3,16 @@ const leapYears = function(year) {
     // else if year is divisble by 100 and 400 return true
     // else if year is divisble by 400 return true
     //else return false
+    //1900 = false
     //1600 = true
+    //700 = false
 
-    if (year % 4 == 0 && year % 100 == 0) {
+
+    if (year % 100 == 0 && year % 400 == 0) {
+        return true;
+    }else if (year % 4 == 0 && year % 100 == 0) {
         return false;
     }else if (year % 4 == 0) {
-        return true;
-    }else if (year % 100 == 0 && year % 400 == 0) {
         return true;
     }else if (year % 400 == 0) {
         return true;

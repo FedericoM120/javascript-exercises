c4964d0dfc06428a3a04981cf053d268d01a07bc
Federico Monteverdi <monteverdi.102@gmail.com>
2022-03-22 22:48:36 -0500

    functions convert farenheit to celsius and vice versa and also round number to one decimal place

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -7,7 +7,6 @@ const leapYears = function(year) {
     //1600 = true
     //700 = false
 
-
     if (year % 100 == 0 && year % 400 == 0) {
         return true;
     }else if (year % 4 == 0 && year % 100 == 0) {
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celsiusUnrounded = (farenheit - 32) * (5 / 9);
+  let celsius = Math.round(celsiusUnrounded * 10) / 10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let farenheitUnrounded = (celsius * (9 / 5)) + 32;
+  let farenheit = Math.round(farenheitUnrounded * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
